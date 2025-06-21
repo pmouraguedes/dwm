45edbd5f1f0bc7b91280ae0d79eb8efe9ef304eb
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-06-21 23:20:31 +0200

    fix formatting screw up

--- config.h
+++ config.h
@@ -14,8 +14,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;        /* 0 means no systray */
 static const int showbar                 = 1;        /* 0 means no bar */
 static const int topbar                  = 1;        /* 0 means bottom bar */
-static const char *fonts[]               = { "JetBrainsMono Nerd Font:size = 11", "Noto Sans:size = 11", "Noto Color Emoji:size = 16" };
-static const char dmenufont[]            = "monospace:size                 = 11";
+static const char *fonts[]               = { "JetBrainsMono Nerd Font:size=11", "Noto Sans:size=11", "Noto Color Emoji:size=16" };
+static const char dmenufont[]            = "monospace:size=11";
 static const char col_gray1[]            = "#222222";
 static const char col_gray2[]            = "#444444";
 static const char col_gray3[]            = "#bbbbbb";
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *runcmd[] = { "rofi", "-show", "run", "-show-icons", NULL };
-static const char *termcmd[]  = { "ghostty", NULL };
+static const char *termcmd[]  = { TERMINAL, NULL };
 static const char *roficlip_cmd[]  = { "rofi-clip", NULL };
 static const char *rofipass_cmd[]  = { "rofi-pass", NULL };
 static const char *volupcmd[] = { "wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "0.05+", NULL };
@@ -103,7 +103,7 @@ static const Key keys[] = {
     { ControlMask|Mod4Mask,               XK_p,                    spawn,          {.v = rofipass_cmd } },
     { MODKEY|ShiftMask,                   XK_Return,               spawn,          {.v = termcmd } },
     // { ControlMask|Mod4Mask,            XK_c,                    spawn,          SHCMD("rofi-clip") },
-    { MODKEY,                             XK_w,                    spawn,          {.v = (const char*[]){ BROWSER, NULL } } },
+    { MODKEY,                             XK_w,                    spawn,          {.v = (const char*[]){ BROWSER,  NULL } } },
     { MODKEY,                             XK_s,                    togglesticky,   {0} },
 	{ MODKEY,                             XK_d,                    spawn,          {.v = dmenucmd } },
 	{ MODKEY,                             XK_F2,                   spawn,          {.v = runcmd } },
@@ -118,15 +118,15 @@ static const Key keys[] = {
 	{ MODKEY,                             XK_space,                zoom,           {0} },
 	{ MODKEY,                             XK_Tab,                  view,           {0} },
 	{ MODKEY,                             XK_q,                    killclient,     {0} },
-	{ MODKEY|ShiftMask,                   XK_m,                    setlayout,      {.v = &layouts[0]} }, // monocle
-	{ MODKEY|ShiftMask,                   XK_g,                    setlayout,      {.v = &layouts[1]} }, // grid
-    { MODKEY|ShiftMask,                   XK_t,                    setlayout,      {.v = &layouts[2]} }, // tile
-    { MODKEY|ShiftMask,                   XK_f,                    setlayout,      {.v = &layouts[3]} }, // floating
-    { MODKEY,                   XK_c,                    setlayout,      {.v = &layouts[4]} }, // centeredmaster
-    { MODKEY|ShiftMask,                   XK_c,                    setlayout,      {.v = &layouts[5]} }, // centeredfloatingmaster
-    { MODKEY|ShiftMask,                   XK_d,                    setlayout,      {.v = &layouts[6]} }, // deck
-    { MODKEY|ShiftMask,                   XK_u,                    setlayout,      {.v = &layouts[7]} }, // bstack
-    { MODKEY|ShiftMask,                   XK_o,                    setlayout,      {.v = &layouts[8]} }, // bstackhoriz
+	{ MODKEY|ShiftMask,                   XK_m,                    setlayout,      {.v = &layouts[0]} },            // monocle
+	{ MODKEY|ShiftMask,                   XK_g,                    setlayout,      {.v = &layouts[1]} },            // grid
+    { MODKEY|ShiftMask,                   XK_t,                    setlayout,      {.v = &layouts[2]} },            // tile
+    { MODKEY|ShiftMask,                   XK_f,                    setlayout,      {.v = &layouts[3]} },            // floating
+    { MODKEY,                             XK_c,                    setlayout,      {.v = &layouts[4]} },            // centeredmaster
+    { MODKEY|ShiftMask,                   XK_c,                    setlayout,      {.v = &layouts[5]} },            // centeredfloatingmaster
+    { MODKEY|ShiftMask,                   XK_d,                    setlayout,      {.v = &layouts[6]} },            // deck
+    { MODKEY|ShiftMask,                   XK_u,                    setlayout,      {.v = &layouts[7]} },            // bstack
+    { MODKEY|ShiftMask,                   XK_o,                    setlayout,      {.v = &layouts[8]} },            // bstackhoriz
 	{ MODKEY|ControlMask,                 XK_comma,                cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,                 XK_period,               cyclelayout,    {.i = +1 } },
 	// { MODKEY,                          XK_space,                setlayout,      {0} },
@@ -147,8 +147,11 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8, 7)
 	TAGKEYS(                        XK_9, 8)
 	{ MODKEY|ShiftMask,                   XK_q,                    quit,           {0} },
-    { MODKEY,                             XK_h,                    setmfact,       {.f = -0.05} },                 // Shrink master window
-    { MODKEY,                             XK_l,                    setmfact,       {.f = +0.05} },                 // Grow master window
+    { MODKEY,                             XK_h,                    setmfact,       {.f = -0.05} },                  // Shrink master window
+    { MODKEY,                             XK_l,                    setmfact,       {.f = +0.05} },                  // Grow master window
+	{ MODKEY|ShiftMask,                   XK_r,                    spawn,          {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
+	{ MODKEY,                   XK_r,                    spawn,          {.v = (const char*[]){ TERMINAL, "-e", "yazi", NULL } } },
+	{ MODKEY|ShiftMask,                   XK_w,                    spawn,          {.v = (const char*[]){ TERMINAL, "-e", "connmanctl", NULL } } },
 };
 
 /* button definitions */
