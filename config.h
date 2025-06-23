a41a1ea352335d251f863cc885f65ce3a48357ca
Paulo Moura Guedes <pmouraguedes@gmail.com>
2025-06-23 14:18:29 +0200

    ewmh patch

--- config.h
+++ config.h
@@ -50,7 +50,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
